26ca92781111e86e7d7d43503d4023de54bb02a0
GitHub Action <action@github.com>
2023-09-29 20:24:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/fishermans/sfdtheme/services/MyProjectService.kt
+++ src/main/kotlin/com/github/fishermans/sfdtheme/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.fishermans.sfdtheme.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.fishermans.sfdtheme.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
